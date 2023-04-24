1c0bfcbd09bdb81854608eead3c8097fda43f009
nkruger <nicolas.nkruger@gmail.com>
2023-04-24 15:51:59 -0300

    feat: :sparkles: no null

---
+++ 004assetions/001non-null-assetions.ts
@@ -0,0 +1,12 @@
+type Entity = {
+  name: string;
+};
+
+// Compiled with --strictNullChecks
+function validateEntity(e?: Entity) {
+  // Throw exception if e is null or invalid entity
+}
+function processEntity(e?: Entity) {
+  validateEntity(e);
+  let s = e!.name; // Assert that e is non-null and access name
+}
